deb1fdf69500d727478427603d153cde4a81198f
Neyl <neyltv@gmail.com>
2024-12-08 15:51:51 +0100

    724 events

---
+++ src/helpers/Date/index.js
@@ -0,0 +1,18 @@
+// BUG N°4 le helper getMonth renvoie le bon mois ;
+// l'objet commence a 1 plutot que 0
+export const MONTHS = {
+  0: "janvier",
+  1: "février",
+  2: "mars",
+  3: "avril",
+  4: "mai",
+  5: "juin",
+  6: "juillet",
+  7: "août",
+  8: "septembre",
+  9: "octobre",
+  10: "novembre",
+  11: "décembre",
+};
+
+export const getMonth = (date) => MONTHS[date.getMonth()];
